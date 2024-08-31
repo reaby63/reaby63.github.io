b5f4d31c6ccae91b7614d55f2a9caeb881534ed7
reaby63 <55023816+reaby63@users.noreply.github.com>
2024-08-31 17:09:29 +0800

    123

--- tainrice_web/js/fabein.js
+++ tainrice_web/js/fabein.js
@@ -1,4 +1,32 @@
-// 測試用
-document.querySelector('button').addEventListener('click',function(){
-    alert('ok');
-})
+$(document).ready(function(){
+    // 這段是用來確保瀏覽器在載入JQ後(讓你的網頁有JQ的功能)，才執行裡面的內容，JQ一定要寫在這裡面喔!!
+    /* $('').hide(); */
+    
+});
+
+
+function gdjz(div,cssname,offset){  
+    var a,b,c,d;  
+    d=$(div).offset().top;  
+    a=eval(d + offset);  
+    b=$(window).scrollTop();   
+    c=$(window).height();  
+    if(b+c>a){  
+        $((div)).addClass((cssname));  
+        }  
+    }  
+$(document).ready(function(e) {  
+$(window).scroll(function(){  
+    gdjz("#dh",'xz',100);  
+    }  
+/*var a,b,c;  
+a=$("#dh").offset().top;//元素相對於視窗的距離  
+b=$(window).scrollTop(); //監控視窗已滾動的距離;  
+c=$(document).height();//整個文件的高度  
+d=$(window).height();//瀏覽器視窗的高度*/  
+/*if(d+b>a+100){  
+    $("#dh").addClass("xz");  
+    }  
+*/  
+    );  
+});  
