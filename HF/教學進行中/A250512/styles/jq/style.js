8c5e66eb8a8ad5d0a25aaf0d6d6d48caf8b77f67
reaby63 <55023816+reaby63@users.noreply.github.com>
2025-11-25 15:44:40 +0800

    asdasdas

--- HF/教學進行中/A250512/styles/jq/style.js
+++
@@ -1,8 +0,0 @@
-
-
-$(document).ready(function () { 
-    $('.qa_box_title').click(function () { 
-        $(this).next('.qa_box_content').slideToggle();
-        // $('.qa_box_content').slideToggle();
-    })
-})
