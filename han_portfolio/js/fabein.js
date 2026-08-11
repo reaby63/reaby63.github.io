5e50d8f55f30cd0442aa81a2bf9259b2f5ff9a7f
reaby63 <55023816+reaby63@users.noreply.github.com>
2026-08-11 22:38:08 +0800

    bug啊啊啊

--- han_portfolio/js/fabein.js
+++ han_portfolio/js/fabein.js
@@ -1,41 +1,3 @@
-$(document).ready(function(){
-    // 這段是用來確保瀏覽器在載入JQ後(讓你的網頁有JQ的功能)，才執行裡面的內容，JQ一定要寫在這裡面喔!!
-    /* $('').hide(); */
-    
-});
-
-// 這段是只有進入時有加動態 就沒惹
-// function fadein_setting(div,cssname,offset){  
-//     var a,b,c,d;  
-//     d=$(div).offset().top;  
-//     a=eval(d + offset);  
-//     b=$(window).scrollTop();   
-//     c=$(window).height();  
-//     if(b+c>a){  
-//         $((div)).addClass((cssname));  
-//         }  
-//     }  
-// $(document).ready(function(e) {  
-//     $(window).scroll(function(){  
-//         fadein_setting("#work_title",'fadein_r_l',100);
-//         fadein_setting("#app_left",'fadein_bot_top',200);
-//         fadein_setting("#app_right",'fadein_bot_top',400);
-//         fadein_setting("#illus",'fadein_bot_top',200);
-//         fadein_setting("#footer",'fadein_bot_top',200);
-//         }  
-//     /*var a,b,c;  
-//     a=$("#dh").offset().top;//元素相對於視窗的距離  
-//     b=$(window).scrollTop(); //監控視窗已滾動的距離;  
-//     c=$(document).height();//整個文件的高度  
-//     d=$(window).height();//瀏覽器視窗的高度*/  
-//     /*if(d+b>a+100){  
-//         $("#dh").addClass("xz");  
-//         }  
-//     */  
-//     );  
-// }); 
-
-// 這段是 進入畫面時加動態 移出畫面後移除動態~
 $(document).ready(function () {
 
     function observeAnimation(selector, fadeInClass) {
@@ -46,24 +8,36 @@ $(document).ready(function () {
 
             entries.forEach(function (entry) {
 
+                const $target = $(entry.target);
+
                 if (entry.isIntersecting) {
 
-                    $(entry.target)
-                        .removeClass(fadeOutClass)
-                        .addClass(fadeInClass);
+                    // 進入畫面才執行 fadein
+                    if (!$target.hasClass(fadeInClass)) {
+
+                        $target
+                            .removeClass(fadeOutClass)
+                            .addClass(fadeInClass);
+
+                    }
 
                 } else {
 
-                    $(entry.target)
-                        .removeClass(fadeInClass)
-                        .addClass(fadeOutClass);
+                    // 完全離開畫面才執行 fadeout
+                    if (!$target.hasClass(fadeOutClass)) {
+
+                        $target
+                            .removeClass(fadeInClass)
+                            .addClass(fadeOutClass);
+
+                    }
 
                 }
 
             });
 
         }, {
-            threshold: 0.2
+            threshold: 0
         });
 
         $(selector).each(function () {
